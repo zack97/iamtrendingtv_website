47896259d58d95ec614f5fa8dabf82aed8152be1
zack10 <tshibzack@gmail.com>
2025-03-18 16:47:13 +0100

    2 commit

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Chargement de la dernière vidéo YouTube
-  fetch(
-    "https://www.googleapis.com/youtube/v3/search?key=YOUR_API_KEY&channelId=YOUR_CHANNEL_ID&part=snippet,id&order=date&maxResults=1"
-  )
+
+  const apiKey = "AIzaSyAtPwKlAuIygPcACS_0Em_GuJ6T2kmNRZ8";
+  const channelId = "UCSyTH1vIjPP3sVoYSRSrAOg";
+  const apiUrl = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelId}&part=snippet,id&order=date&maxResults=1`;
+
+  fetch(apiUrl) // Utilisation de la bonne URL API
     .then((response) => response.json())
     .then((data) => {
       let videoId = data.items[0].id.videoId;
